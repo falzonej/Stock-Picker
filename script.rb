d96fa67bd8b599c101aa44933640a379fa24961b
JoeFalzone <falzonej@gmail.com>
2023-02-07 15:42:42 -0500

    add core idea of trimming array to find minmax vals

--- script.rb
+++ script.rb
@@ -1,1 +1,6 @@
-first commit  
+current_week_prices = [17,3,6,9,15,8,6,1,10,4]
+
+min_price = current_week_prices.min()
+min_price_index = current_week_prices.find_index(min_price)
+sliced_prices = current_week_prices.slice(min_price_index, current_week_prices.length)
+p sliced_prices.minmax()
