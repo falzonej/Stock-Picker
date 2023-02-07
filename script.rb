dc5c8fbf6d6c596ef364754fd472af944884e45d
JoeFalzone <falzonej@gmail.com>
2023-02-07 16:42:22 -0500

    add slice_array method

--- script.rb
+++ script.rb
@@ -1,6 +1,13 @@
 current_week_prices = [17,3,6,9,15,8,6,1,10,4]
 
-min_price = current_week_prices.min()
-min_price_index = current_week_prices.find_index(min_price)
-sliced_prices = current_week_prices.slice(min_price_index, current_week_prices.length)
-p sliced_prices.minmax()
+#min_price = current_week_prices.min()
+#min_price_index = current_week_prices.find_index(current_week_prices.min())
+sliced_prices = current_week_prices.slice(current_week_prices.find_index(current_week_prices.min()), current_week_prices.length)
+sliced_prices.minmax()
+
+def slice_array(array)
+  sliced_array = array.slice(array.find_index(array.min()), array.length)
+  sliced_array.minmax
+end
+
+p slice_array(current_week_prices)
